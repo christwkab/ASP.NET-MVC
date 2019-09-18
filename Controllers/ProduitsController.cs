b0f4515e09d24f532c380ee466f4efe4f328ca87
Christian KABORE <christwkab@gmail.com>
2019-09-18 01:09:09 -0400

    ASP.NET MVC

---
+++ Controllers/ProduitsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EpicerieMvcEF;
+
+namespace EpicerieMvcEF.Controllers
+{
+    public class ProduitsController : Controller
+    {
+        private EpicerieEntities db = new EpicerieEntities();
+
+        // GET: Produits
+        public ActionResult Index()
+        {
+            var produits = db.Produits.Include(p => p.Fournisseur);
+            return View(produits.ToList());
+        }
+
+        // GET: Produits/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produit produit = db.Produits.Find(id);
+            if (produit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produit);
+        }
+
+        // GET: Produits/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDFournisseur = new SelectList(db.Fournisseurs, "ID", "Nom");
+            return View();
+        }
+
+        // POST: Produits/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Designation,Prix,Qte,IDFournisseur")] Produit produit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produits.Add(produit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDFournisseur = new SelectList(db.Fournisseurs, "ID", "Nom", produit.IDFournisseur);
+            return View(produit);
+        }
+
+        // GET: Produits/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produit produit = db.Produits.Find(id);
+            if (produit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDFournisseur = new SelectList(db.Fournisseurs, "ID", "Nom", produit.IDFournisseur);
+            return View(produit);
+        }
+
+        // POST: Produits/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Designation,Prix,Qte,IDFournisseur")] Produit produit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDFournisseur = new SelectList(db.Fournisseurs, "ID", "Nom", produit.IDFournisseur);
+            return View(produit);
+        }
+
+        // GET: Produits/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produit produit = db.Produits.Find(id);
+            if (produit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produit);
+        }
+
+        // POST: Produits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produit produit = db.Produits.Find(id);
+            db.Produits.Remove(produit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
